0fbe779cd781b5fb77a9382448145d62d2bf12b2
Arisen32 <urielgutierrez490@gmail.com>
2025-06-27 16:38:49 -0600

    actualizacion del archivo settings 2

--- innovatube/settings.py
+++ innovatube/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 LOGIN_URL = '/login/'
 
 AUTH_USER_MODEL = 'youtubefav.User'
